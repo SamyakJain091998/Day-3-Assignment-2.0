61f31cb1b21db32109ccb0ca7eee506f5bc9109e
SamyakJain091998 <2016224@iiitdmj.ac.in>
2020-09-22 18:36:04 +0530

    UC4 & UC5 executed successfully. Both in the same code set...

---
+++ UC4.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class UC4{
+
+	static final int no_play = 0;
+	static final int ladder = 1;
+	static final int snake = 2;
+
+	public static void main(String[] args){
+		int min=1;
+		int max=6;
+		Random rand=new Random();
+		// int roll=rand.nextInt( (max-min) + 1 ) + min;
+		int position = 0;
+		// double option=Math.floor(Math.random() * 10)%3;
+
+		while(position!=100)
+			{	
+				int roll=rand.nextInt( (max-min) + 1 ) + min;
+				System.out.println("Dice roll came out to be: " + roll);
+				//int position = 0;
+				double option=Math.floor(Math.random() * 10)%3;
+
+				switch((int) option){
+					case no_play:
+						System.out.println("Sorry player! You got no_play option. You stay here at position: " + position);
+						break;
+					case ladder:
+						position+=roll;
+						if(position>100){
+							position-=roll;
+							System.out.println("Hey! You got ladder option but Position can't exceed 100. Please play again.");
+							break;
+						}
+						System.out.println("Kudos player! You got the ladder option. You march forward..");
+						System.out.println("Your updated position would be: " + position);
+						break;
+					case snake:
+						System.out.println("Oops! You just got bit by a snake. You go backwards..");
+						position-=roll;
+						if(position<0){
+							position = 0;
+						}
+						System.out.println("Your updated position would be: " + position);
+						break;
+					default: 
+						System.out.println("Nothing happens here.");
+				}
+			}
+			System.out.println();
+			System.out.println("Wohoo! You just reached position 100. Congratulation. You may now exit the game or play again...");
+	}
+}
